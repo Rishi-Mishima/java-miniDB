da904d32dd1f461ff4ed0dc39467e277d1791acf
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-12 22:15:07 +0200

    feat(common): error message added

--- src/main/java/top/risha/minidb/backend/tm/TransactionManager.java
+++ src/main/java/top/risha/minidb/backend/tm/TransactionManager.java
@@ -1,5 +1,16 @@
 package top.risha.minidb.backend.tm;
 
+import top.risha.minidb.backend.utils.Panic;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import top.risha.minidb.common.Error;
+
 /**
  * TransactionManager 提供了一些接口供其他模块调用，用来创建事务和查询事务状态。更具体的：
  */
@@ -11,4 +22,38 @@ public interface TransactionManager {
     boolean isCommitted(long xid);      // 查询一个事务的状态是否是已提交
     boolean isAborted(long xid);        // 查询一个事务的状态是否是已取消
     void close();
+
+    public static TransactionManagerImpl create(String path) {
+        File f = new File(path+TransactionManagerImpl.XID_SUFFIX);
+        try {
+            if(!f.createNewFile()) {
+                Panic.panic(Error.FileExistsException);
+            }
+        } catch (Exception e) {
+            Panic.panic(e);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Error.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+
+        // 写空XID文件头
+        ByteBuffer buf = ByteBuffer.wrap(new byte[TransactionManagerImpl.LEN_XID_HEADER_LENGTH]);
+        try {
+            fc.position(0);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+
+        return new TransactionManagerImpl(raf, fc);
+    }
 }
