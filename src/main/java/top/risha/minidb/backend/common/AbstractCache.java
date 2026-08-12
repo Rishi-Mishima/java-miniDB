9d83cb704de991ea03774ac8a215c77703c5bd30
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-13 00:03:25 +0200

    feat(cache): implement cache foundation and clean project setup

---
+++ src/main/java/top/risha/minidb/backend/common/AbstractCache.java
@@ -0,0 +1,29 @@
+package top.risha.minidb.backend.common;
+
+
+import java.util.HashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * AbstractCache 实现了一个引用计数策略的缓存
+ */
+public class AbstractCache<T> {
+    private HashMap<Long, T> cache;                     // 实际缓存的数据
+    private HashMap<Long, Integer> references;          // 元素的引用个数
+    private HashMap<Long, Boolean> getting;             // 正在获取某资源的线程
+
+    private int maxResource;                            // 缓存的最大缓存资源数
+    private int count = 0;                              // 缓存中元素的个数
+    private Lock lock;
+
+    public AbstractCache(int maxResource) {
+        this.maxResource = maxResource;
+        cache = new HashMap<>();
+        references = new HashMap<>();
+        getting = new HashMap<>();
+        lock = new ReentrantLock();
+    }
+
+
+}
