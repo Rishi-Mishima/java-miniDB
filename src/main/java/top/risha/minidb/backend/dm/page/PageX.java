6d515a59bd05bf8d7b017f4615384feba40a88bf
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-13 23:40:44 +0200

    feat(dm): pagex implements fso related function

--- src/main/java/top/risha/minidb/backend/dm/page/PageX.java
+++ src/main/java/top/risha/minidb/backend/dm/page/PageX.java
@@ -3,12 +3,15 @@ package top.risha.minidb.backend.dm.page;
 import top.risha.minidb.backend.dm.pageCache.PageCache;
 import top.risha.minidb.backend.utils.Parser;
 
+import java.util.Arrays;
+
 /**
  * PageX管理普通页
  */
 public class PageX {
-    private static final short OF_FREE = 0;
-    private static final short OF_DATA = 2;
+
+    private static final short OF_FREE = 0; // FSO 在页面中的起始偏移位置，常数 0
+    private static final short OF_DATA = 2; // ：FSO 占用的字节长度，常数 2
     public static final int MAX_FREE_SPACE = PageCache.PAGE_SIZE - OF_DATA;
 
     public static byte[] initRaw() {
@@ -21,6 +24,24 @@ public class PageX {
         System.arraycopy(Parser.short2Byte(ofData),0, raw, OF_FREE, OF_DATA);
     }
 
+    // 获取 pg 的 FSO
+    public static short getFSO(Page pg) {
+        return getFSO(pg.getData());
+    }
+
+    // 标出当前数据页位置, 从偏移量之后写
+    private static short getFSO(byte[] raw) {
+        // Arrays.copyOfRange(raw, 0, 2) 从页面的字节数组中截取最开头的 2 个字节（索引 0 和 1）。
+        // 将这 2 个 byte 传入 Parser.parseShort()，还原出一个 short 类型的整数。
+        return Parser.parseShort(Arrays.copyOfRange(raw, 0, 2));
+    }
+
+    // 获取页面的空闲空间大小
+    public static int getFreeSpace(Page pg) {
+        // 用整页的总容量减去当前的 FSO
+        return PageCache.PAGE_SIZE - (int)getFSO(pg.getData());
+    }
+
     // 将raw插入pg中，返回插入位置
     public static short insert(Page pg, byte[] raw) {
         pg.setDirty(true);
@@ -29,5 +50,5 @@ public class PageX {
         setFSO(pg.getData(), (short)(offset + raw.length));
         return offset;
     }
-    
+
 }
