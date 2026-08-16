1f3c0f19dfa9df5b1c34b3a67584b3aa0470c292
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-16 20:10:13 +0200

    feat(vm): implements repeatableRead

--- src/main/java/top/risha/minidb/backend/vm/Visibility.java
+++ src/main/java/top/risha/minidb/backend/vm/Visibility.java
@@ -20,4 +20,23 @@ public class Visibility {
         }
         return false;
     }
+
+
+    // RR implementation
+    private static boolean repeatableRead(TransactionManager tm, Transaction t, Entry e) {
+        long xid = t.xid;
+        long xmin = e.getXmin();
+        long xmax = e.getXmax();
+        if(xmin == xid && xmax == 0) return true;
+
+        if(tm.isCommitted(xmin) && xmin < xid && !t.isInSnapshot(xmin)) {
+            if(xmax == 0) return true;
+            if(xmax != xid) {
+                if(!tm.isCommitted(xmax) || xmax > xid || t.isInSnapshot(xmax)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
