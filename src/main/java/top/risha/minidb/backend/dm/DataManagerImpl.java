fe6a45f35f2a9d9703d101a5db505ecc22a6630a
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-16 14:53:54 +0200

    feat(dm): datamanagerImpl implements pageIndex

--- src/main/java/top/risha/minidb/backend/dm/DataManagerImpl.java
+++ src/main/java/top/risha/minidb/backend/dm/DataManagerImpl.java
@@ -1,12 +1,55 @@
 package top.risha.minidb.backend.dm;
 
 import top.risha.minidb.backend.dm.dataItem.DataItemImpl;
+import top.risha.minidb.backend.dm.logger.Logger;
+import top.risha.minidb.backend.dm.page.Page;
+import top.risha.minidb.backend.dm.page.PageX;
+import top.risha.minidb.backend.dm.pageCache.PageCache;
+import top.risha.minidb.backend.dm.pageIndex.PageIndex;
+import top.risha.minidb.backend.tm.TransactionManager;
+import top.risha.minidb.backend.utils.Panic;
 
 public class DataManagerImpl {
+
+
+    TransactionManager tm;
+    PageCache pc;
+    Logger logger;
+    PageIndex pIndex;
+    Page pageOne;
+
+    public DataManagerImpl(PageCache pc, Logger logger, TransactionManager tm) {
+        super();
+        this.pc = pc;
+        this.logger = logger;
+        this.tm = tm;
+        this.pIndex = new PageIndex();
+    }
+
     public void logDataItem(long xid, DataItemImpl dataItem) {
     }
 
     public void releaseDataItem(DataItemImpl dataItem) {
 
+    }
+
+    // 初始化pageIndex
+    void fillPageIndex() {
+        int pageNumber = pc.getPageNumber();
+        for(int i = 2; i <= pageNumber; i ++) {
+            Page pg = null;
+            try {
+                // 根据页号 i 从 PageCache 获取 Page。
+                pg = pc.getPage(i);
+            } catch (Exception e) {
+                Panic.panic(e);
+            }
+            pIndex.add(
+                    // 获取页号。
+                    pg.getPageNumber(),
+                    // 计算这个 Page 还有多少空间。
+                    PageX.getFreeSpace(pg));
+            pg.release();
+        }
     }
 }
--- src/main/java/top/risha/minidb/backend/dm/pageIndex/PageIndex.java
+++ src/main/java/top/risha/minidb/backend/dm/pageIndex/PageIndex.java
@@ -3,6 +3,7 @@ package top.risha.minidb.backend.dm.pageIndex;
 import top.risha.minidb.backend.dm.pageCache.PageCache;
 
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 
 public class PageIndex {
     // 将一页划成 40 个区间
@@ -10,4 +11,39 @@ public class PageIndex {
     private static final int THRESHOLD = PageCache.PAGE_SIZE / INTERVALS_NO;
 
     private List[] lists;
+
+    private Lock lock;
+
+
+
+    // 从 PageIndex 中获取页面，算出区间号，直接取即可 - 空闲空间页面选择器（Page Selector）。
+    // 当需要向数据库插入一条新数据（长度为 spaceSize）时，快速在内存中挑选出一个“剩余空间足够”的数据页（Page）。
+    public PageInfo select(int spaceSize) {
+        lock.lock();
+        try {
+            int number = spaceSize / THRESHOLD;
+            if(number < INTERVALS_NO) number ++;
+            while(number <= INTERVALS_NO) {
+                if(lists[number].size() == 0) {
+                    number ++;
+                    continue;
+                }
+                return (PageInfo) lists[number].remove(0);
+            }
+            return null;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void add(int pgno, int freeSpace) {
+        lock.lock();
+        try {
+            int number = freeSpace / THRESHOLD;
+            lists[number].add(new PageInfo(pgno, freeSpace));
+        } finally {
+            lock.unlock();
+        }
+    }
+
 }
