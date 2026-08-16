3b6d53bdce96967aeebcb9c8efa98600259512d2
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-16 20:03:52 +0200

    feat(vm): transction implements the snapshot function

--- src/main/java/top/risha/minidb/backend/vm/Transaction.java
+++ src/main/java/top/risha/minidb/backend/vm/Transaction.java
@@ -1,5 +1,37 @@
 package top.risha.minidb.backend.vm;
 
+import top.risha.minidb.backend.tm.TransactionManagerImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Snap快照
+ */
 public class Transaction {
     public long xid;
+    public int level; // 隔离级别
+    public Map<Long, Boolean> snapshot;
+    public Exception err;
+    public boolean autoAborted;
+
+    public static Transaction newTransaction(long xid, int level, Map<Long, Transaction> active) {
+        Transaction t = new Transaction();
+        t.xid = xid;
+        t.level = level;
+        if(level != 0) {
+            t.snapshot = new HashMap<>();
+            for(Long x : active.keySet()) {
+                t.snapshot.put(x, true);
+            }
+        }
+        return t;
+    }
+
+    public boolean isInSnapshot(long xid) {
+        if(xid == TransactionManagerImpl.SUPER_XID) {
+            return false;
+        }
+        return snapshot.containsKey(xid);
+    }
 }
